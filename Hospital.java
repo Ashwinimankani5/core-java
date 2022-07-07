451730dc49d8920df15a99df3010a596e148389b
Ashwinimankani5 <107829572+Ashwinimankani5@users.noreply.github.com>
2022-07-07 22:26:28 +0530

    Add files via upload

--- Hospital.java
+++ Hospital.java
@@ -1,79 +1,38 @@
-class Hospital{
+class Hospital
+{
+	//has many patients
+	PatientDTO[] dto = new PatientDTO[5];
 
- String name;													
- String address;
- long contactNo;
- String ownerName;
- String[]currentHospitals={"Sandhyashree","Manipal","Annexure1"};
-  String[]currentHospitals1={" Apollo","City","Vishal"};
-
-public static void main (String a[]){
-//className refvariables=new className();
-   Hospital hospital=new Hospital();
-    hospital.name="Sandhyashree";
-    hospital.address="Rajajinagar";
-	hospital.contactNo=8123808055L;
-  hospital.ownerName="KiranRao";
-    System.out.println("The currentHospitals is====");
-    hospital.getCurrentHospitals();
+	//instance variable
+	int index;
 	
-	      System.out.println("The hospital name" +hospital.name);
-	  	  System.out.println("The hospital address" +hospital.address);
-	      System.out.println("The hospital contact No" +hospital.contactNo);
-          System.out.println("The hospital owner Name" +hospital.ownerName);
-
-		  
-	Hospital hospital1=new Hospital();	  
-	hospital1.name="Manipal";
-    hospital1.address="Shvajinagar";
-	hospital1.contactNo=8123808006L;
-	hospital1.ownerName="Laxmi";
-    System.out.println("The currentHospitals is====");
-    hospital1.getCurrentHospitals1();
-		  
-		  System.out.println("The hospital1 name" +hospital1.name);
-	  	  System.out.println("The hospital1 address" +hospital1.address);
-	      System.out.println("The hospital1 contact No" +hospital1.contactNo);
-          System.out.println("The hospital1 owner Name" +hospital1.ownerName);
-		  
+	public Hospital()
+	{
+		System.out.println("Hospital constant is created");
 	}
-   public void getCurrentHospitals(){
-	for (int z=0; z<currentHospitals.length; z++){
-				
-			System.out.println(currentHospitals[z]);
-				
-
-	}
-			
-	}
-     public void getCurrentHospitals1(){
-	 for (int z=1;  z<currentHospitals.length; z++){
-	   System.out.println(currentHospitals[z]);
-	 
-	 }
-	 
-	 }
 	
-	
-	 }
-	   
-	   
-   
-
-		
-		
-		
-
-
-
-
-
-
-
-
-
-
- 
- 	
-		
-
+	public boolean createPatient(PatientDTO dto)
+	{
+		System.out.println("inside create patient");	
+		boolean isAdded = false;
+		if(dto != null && dto.getName() != null)
+		{
+			this.dto[index++]  = dto;
+			//index++;
+			isAdded = true;
+		}
+		else {
+			 System.out.println("can not add patient details");
+		}
+		return isAdded;
+	}
+	public void getPatientDetails()
+	{
+		System.out.println("hospital get the details of patient");
+			 for(int  i=0; i<dto.length;i++)
+			 {
+				 System.out.println(dto[i].getId()+ "  " +dto[i].getName() + "  " +dto[i].getAddress() 
+				 + "  " +dto[i].getGender() + " " +dto[i].getContactNo()+ "  ");
+			 }
+	}
+}
---
+++ HospitalTester.java
@@ -0,0 +1,62 @@
+class HospitalTester
+{
+	public static void main(String args[]){
+		
+	
+	 Hospital hos = new Hospital();
+	 
+	 PatientDTO dto = new PatientDTO();
+	 
+	dto.setId(5586);
+	dto.setName("Rakhesh");
+	dto.setAddress("Bagalkot");
+	dto.setGender(Gender.male);
+	dto.setContactNo(9740096435L);
+	
+	hos.createPatient(dto);
+	
+		 PatientDTO dto1 = new PatientDTO();
+	 
+	dto1.setId(7647);
+	dto1.setName("prakruti");
+	dto1.setAddress("Bilagi");
+	dto1.setGender(Gender.female);
+	dto1.setContactNo(8147542581L);
+	hos.createPatient(dto1);
+	
+			 PatientDTO dto4 = new PatientDTO();
+	 
+	dto4.setId(3387);
+	dto4.setName("prabhas");
+	dto4.setAddress("navanagar");
+	dto4.setGender(Gender.male);
+	dto4.setContactNo(4875128951L);
+	hos.createPatient(dto4);
+			 PatientDTO dto2 = new PatientDTO();
+	 
+	dto2.setId(9748);
+	dto2.setName("ramleela");
+	dto2.setAddress("Banglore");
+	dto2.setGender(Gender.Transgender);
+	dto2.setContactNo(6684572159L);
+	hos.createPatient(dto2);
+			 PatientDTO dto3 = new PatientDTO();
+	 
+	dto3.setId(8748);
+	dto3.setName("padmavati");
+	dto3.setAddress("belagum");
+	dto3.setGender(Gender.female);
+	dto3.setContactNo(7487261957L);
+	hos.createPatient(dto3);
+	
+	
+	System.out.println(dto.getId()+ "  "  + dto.getName()+ "  "   +dto.getAddress()+ "  "    +dto.getGender()+ "  "   + dto.getContactNo());
+	System.out.println(dto1.getId()+ "  "  + dto1.getName()+ "  "   +dto1.getAddress()+ "  "    +dto1.getGender()+ "  "   + dto1.getContactNo());
+	System.out.println(dto2.getId()+ "  "  + dto2.getName()+ "  "   +dto2.getAddress()+ "  "    +dto2.getGender()+ "  "   + dto2.getContactNo());
+	System.out.println(dto3.getId()+ "  "  + dto3.getName()+ "  "   +dto3.getAddress()+ "  "    +dto3.getGender()+ "  "   + dto3.getContactNo());
+	System.out.println(dto4.getId()+ "  "  + dto4.getName()+ "  "   +dto4.getAddress()+ "  "    +dto4.getGender()+ "  "   + dto4.getContactNo());
+	}
+
+
+	
+}
